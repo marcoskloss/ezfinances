438f204a188c0d296d0677debdfa2e52cf6fb1ec
marcoskloss <marcoskloss2001@gmail.com>
2021-10-10 22:05:18 -0300

    add headers in options

--- test/util/doRequest.ts
+++ test/util/doRequest.ts
@@ -1,12 +1,20 @@
 /* eslint-disable @typescript-eslint/explicit-module-boundary-types */
 import http from 'http';
 
-async function doRequest(url: string, body?: any, opts?: any): Promise<void> {
+async function doRequest(url: string, body?: any, opts?: any): Promise<any> {
     const route = `http://localhost:${process.env.APP_PORT}${url}`;
     const bodyData = JSON.stringify(body);
 
+    const options = {
+        ...opts,
+        headers: {
+            'Content-Type': 'application/json',
+            'Content-Length': body ? Buffer.byteLength(bodyData) : 0,
+        },
+    };
+
     return new Promise((resolve) => {
-        const request = http.request(route, opts, async (res) => {
+        const request = http.request(route, options, async (res) => {
             const chunks: string[] = [];
 
             res.on('data', (chunk) => chunks.push(chunk));
