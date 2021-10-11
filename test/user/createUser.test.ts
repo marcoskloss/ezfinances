6d0df26e6d6b92817388d4542a986255da8cb892
marcoskloss <marcoskloss2001@gmail.com>
2021-10-11 20:36:54 -0300

    ensure that it should return 409 if user already exists

--- test/user/createUser.test.ts
+++ test/user/createUser.test.ts
@@ -7,15 +7,19 @@ describe('/users/create', () => {
     });
 
     describe('When creating a new user', () => {
+        const userData = {
+            name: 'username',
+            password: '123456',
+            email: 'username@mail.com',
+        };
+
         it('should create a new user with success and return 201', async () => {
             const options = {
                 method: Methods.post,
             };
 
             const body = {
-                name: 'username',
-                password: '123456',
-                email: 'username@mail.com',
+                ...userData,
             };
 
             const response = await doRequest('/users/create', body, options);
@@ -23,6 +27,21 @@ describe('/users/create', () => {
             const user = await User.findOne({ email: body.email });
             expect(response.status).toBe(201);
             expect(user).toBeTruthy();
+        });
+
+        it('should return 409 if user already exists', async () => {
+            await new User(userData).save();
+
+            const body = {
+                ...userData,
+            };
+
+            const options = {
+                method: Methods.post,
+            };
+
+            const response = await doRequest('/users/create', body, options);
+            expect(response.status).toBe(409);
         });
     });
 });
