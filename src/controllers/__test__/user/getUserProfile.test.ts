37662d4f9ca54fbae0d4d468559794843d33d266
marcoskloss <marcoskloss2001@gmail.com>
2021-10-17 21:50:38 -0300

    add getUserProfile controller

---
+++ src/controllers/__test__/user/getUserProfile.test.ts
@@ -0,0 +1,50 @@
+import { GetUserProfileController } from '@src/controllers/user/getUserProfile';
+import { AppError } from '@src/errors/appError';
+import { User } from '@src/models/user';
+import { UserRepository } from '@src/repositories/user';
+
+jest.mock('@src/models/user');
+describe('GetUserProfileController', () => {
+    const mockedUserModel = User as jest.Mocked<typeof User>;
+
+    const request: any = {
+        user_decoded: {
+            id: '123',
+        },
+    };
+    const userRepository = new UserRepository(mockedUserModel);
+
+    test("should return token's owner information", async () => {
+        const userData = {
+            id: '123',
+            email: 'email',
+            name: 'username',
+            toJSON() {
+                return { email: this.email, name: this.name };
+            },
+        };
+
+        mockedUserModel.findById.mockResolvedValueOnce(userData as any);
+
+        const getUserProfileController = new GetUserProfileController(
+            userRepository
+        );
+
+        const response = await getUserProfileController.handle(request);
+        expect(response.data).toMatchObject({
+            email: userData.email,
+            name: userData.name,
+        });
+    });
+
+    test('should throw when user is not found', async () => {
+        mockedUserModel.findById.mockResolvedValueOnce(null);
+        const getUserProfileController = new GetUserProfileController(
+            userRepository
+        );
+
+        await expect(
+            getUserProfileController.handle(request)
+        ).rejects.toMatchObject({ code: 404 });
+    });
+});
---
+++ src/controllers/user/getUserProfile.ts
@@ -0,0 +1,23 @@
+import { UserRepository } from '@src/repositories/user';
+import { Request, Response } from '@src/util/http';
+import { Controller } from '@src/controllers/contract';
+import { AppError } from '@src/errors/appError';
+
+export class GetUserProfileController implements Controller {
+    constructor(
+        private userRepository: UserRepository = new UserRepository()
+    ) {}
+
+    async handle(req: Request): Promise<Response> {
+        const id = req.user_decoded?.id;
+        const user = await this.userRepository.model.findById(id);
+
+        if (!user) {
+            throw new AppError('Usuário não encontrado!', 404);
+        }
+
+        return {
+            data: user.toJSON(),
+        };
+    }
+}
