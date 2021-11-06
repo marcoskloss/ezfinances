ecc46e661fd1ede3bfc0bdb2f6fe1b7e7fc2622d
marcoskloss <marcoskloss2001@gmail.com>
2021-11-05 22:16:48 -0300

    add Group model

---
+++ src/models/group.ts
@@ -0,0 +1,28 @@
+import mongoose, { Schema, Document, Model } from 'mongoose';
+
+export interface GroupData {
+    _id?: string;
+    title: string;
+    active: boolean;
+    user: string;
+}
+
+const schema = new mongoose.Schema(
+    {
+        title: { type: String, required: true, maxLength: 20 },
+        active: { type: Boolean, required: true, default: true },
+        user: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+    },
+    {
+        toJSON: {
+            transform: (_, ret) => {
+                ret.id = ret._id;
+                delete ret._id;
+                delete ret.__v;
+            },
+        },
+    }
+);
+
+export interface GroupModel extends Omit<GroupData, '_id'>, Document {}
+export const Group: Model<GroupModel> = mongoose.model('Group', schema);
