5c8da3c4f18a5fbb95f6608d422d559f632ca9ef
marcoskloss <marcoskloss2001@gmail.com>
2021-11-06 19:03:33 -0300

    remove coverageThreshold

--- jest.config.js
+++ jest.config.js
@@ -15,14 +15,6 @@ module.exports = {
     coverageDirectory: 'coverage',
     coverageProvider: 'v8',
     coverageReporters: ['text', 'lcov'],
-    coverageThreshold: {
-        global: {
-            branches: 100,
-            functions: 100,
-            lines: 100,
-            statements: 100,
-        },
-    },
     watchPathIgnorePatterns: ['node_modules'],
     transformIgnorePatterns: ['node_modules'],
     collectCoverageFrom: ['src/**/*.ts', '!src/**/index.ts', '!src/types.d.ts'],
