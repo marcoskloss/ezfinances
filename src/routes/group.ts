7a4a12de6540da1058851c90661444656dfdff26
marcoskloss <marcoskloss2001@gmail.com>
2021-11-06 17:17:10 -0300

    add [PUT] /groups route

--- src/routes/group.ts
+++ src/routes/group.ts
@@ -1,8 +1,10 @@
 import { adaptController } from '@src/controllers/adaptController';
 import { CreateGroupController } from '@src/controllers/group/createGroup';
+import { UpdateGroupController } from '@src/controllers/group/updateGroup';
 import { adaptMiddleware } from '@src/middlewares/adaptMiddleware';
 import { AuthMiddleware } from '@src/middlewares/auth';
 import { CreateGroupValidator } from '@src/middlewares/validators/group/createGroupValidator';
+import { UpdateGroupValidator } from '@src/middlewares/validators/group/updateGroupValidator';
 import { Router } from 'express';
 
 const groupRoute = Router();
@@ -12,6 +14,13 @@ groupRoute.post(
     adaptMiddleware(new AuthMiddleware()),
     adaptMiddleware(new CreateGroupValidator()),
     adaptController(new CreateGroupController())
+);
+
+groupRoute.put(
+    '/',
+    adaptMiddleware(new AuthMiddleware()),
+    adaptMiddleware(new UpdateGroupValidator()),
+    adaptController(new UpdateGroupController())
 );
 
 export { groupRoute };
