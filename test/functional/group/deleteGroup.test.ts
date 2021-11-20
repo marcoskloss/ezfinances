e5f20b2081cefd8cb5a15edbbdfe7313738a82e4
marcoskloss <marcoskloss2001@gmail.com>
2021-11-19 22:47:55 -0300

    ensure that [DELETE] /groups/:groupId should not delete the given group

--- test/functional/group/deleteGroup.test.ts
+++ test/functional/group/deleteGroup.test.ts
@@ -1,12 +1,14 @@
 import { Group } from '@src/models/group';
 import { User, UserModel } from '@src/models/user';
 import { AuthService } from '@src/services/auth';
+import { Transaction } from '@src/models/transaction';
 import { doRequest, Methods } from '../../util/doRequest';
 
 describe('DELETE /groups', () => {
     let user: UserModel;
     beforeAll(async () => {
         await Group.deleteMany({});
+        await Transaction.deleteMany({});
 
         user = new User({
             name: 'username',
@@ -21,6 +23,7 @@ describe('DELETE /groups', () => {
     });
     afterEach(async () => {
         await Group.deleteMany({});
+        await Transaction.deleteMany({});
     });
 
     it('should delete the group and return 200', async () => {
@@ -44,5 +47,36 @@ describe('DELETE /groups', () => {
 
         expect(response.status).toBe(200);
         await expect(Group.findById(group.id)).resolves.toBeFalsy();
+    });
+
+    it('should not delete the group and return 400', async () => {
+        const token = AuthService.generateToken(user.id);
+
+        const options = {
+            method: Methods.delete,
+            headers: {
+                Authorization: `Bearer ${token}`,
+            },
+        };
+
+        const data = await new Group({
+            active: true,
+            title: 'My New Group',
+            user: user.id,
+        }).save();
+        const group = data.toJSON();
+
+        await new Transaction({
+            amount: 1,
+            title: 'foo',
+            user: user.id,
+            group: group.id,
+            date: new Date(),
+        }).save();
+
+        const response = await doRequest(`/groups/${group.id}`, {}, options);
+
+        expect(response.status).toBe(400);
+        await expect(Group.findById(group.id)).resolves.toBeTruthy();
     });
 });
