6056ce989d80deb48ef1a32c8e4af5d21339f035
marcoskloss <marcoskloss2001@gmail.com>
2021-11-20 22:07:03 -0300

    add barrel file for import/export group controllers

---
+++ src/controllers/group/index.ts
@@ -0,0 +1,5 @@
+export { CreateGroupController } from '@src/controllers/group/createGroup';
+export { DeleteGroupController } from '@src/controllers/group/deleteGroup';
+export { ListGroupsController } from '@src/controllers/group/listGroup';
+export { ShowGroupController } from '@src/controllers/group/showGroup';
+export { UpdateGroupController } from '@src/controllers/group/updateGroup';
--- src/routes/group.ts
+++ src/routes/group.ts
@@ -1,15 +1,11 @@
 import { Router } from 'express';
 import { adaptController } from '@src/controllers/adaptController';
-import { CreateGroupController } from '@src/controllers/group/createGroup';
-import { UpdateGroupController } from '@src/controllers/group/updateGroup';
+import * as Group from '@src/controllers/group';
 import { adaptMiddleware } from '@src/middlewares/adaptMiddleware';
 import { AuthMiddleware } from '@src/middlewares/auth';
 import { CreateGroupValidator } from '@src/middlewares/validators/group/createGroupValidator';
 import { UpdateGroupValidator } from '@src/middlewares/validators/group/updateGroupValidator';
-import { ListGroupsController } from '@src/controllers/group/listGroup';
 import { ShowGroupValidator } from '@src/middlewares/validators/group/showGroupValidator';
-import { ShowGroupController } from '@src/controllers/group/showGroup';
-import { DeleteGroupController } from '@src/controllers/group/deleteGroup';
 
 const groupRoute = Router();
 const authMiddleware = new AuthMiddleware();
@@ -18,33 +14,33 @@ groupRoute.post(
     '/',
     adaptMiddleware(authMiddleware),
     adaptMiddleware(new CreateGroupValidator()),
-    adaptController(new CreateGroupController())
+    adaptController(new Group.CreateGroupController())
 );
 
 groupRoute.put(
     '/:groupId',
     adaptMiddleware(authMiddleware),
     adaptMiddleware(new UpdateGroupValidator()),
-    adaptController(new UpdateGroupController())
+    adaptController(new Group.UpdateGroupController())
 );
 
 groupRoute.get(
     '/',
     adaptMiddleware(authMiddleware),
-    adaptController(new ListGroupsController())
+    adaptController(new Group.ListGroupsController())
 );
 
 groupRoute.get(
     '/:groupId',
     adaptMiddleware(authMiddleware),
     adaptMiddleware(new ShowGroupValidator()),
-    adaptController(new ShowGroupController())
+    adaptController(new Group.ShowGroupController())
 );
 
 groupRoute.delete(
     '/:groupId',
     adaptMiddleware(authMiddleware),
-    adaptController(new DeleteGroupController())
+    adaptController(new Group.DeleteGroupController())
 );
 
 export { groupRoute };
