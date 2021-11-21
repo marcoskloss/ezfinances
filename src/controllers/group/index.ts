9f68c190dca593091a0302863ee78d304297ac57
marcoskloss <marcoskloss2001@gmail.com>
2021-11-20 22:19:27 -0300

    fix import

--- src/controllers/group/index.ts
+++ src/controllers/group/index.ts
@@ -1,5 +1,5 @@
 export { CreateGroupController } from '@src/controllers/group/createGroup';
 export { DeleteGroupController } from '@src/controllers/group/deleteGroup';
-export { ListGroupsController } from '@src/controllers/group/listGroup';
+export { ListGroupsController } from '@src/controllers/group/listGroups';
 export { ShowGroupController } from '@src/controllers/group/showGroup';
 export { UpdateGroupController } from '@src/controllers/group/updateGroup';
